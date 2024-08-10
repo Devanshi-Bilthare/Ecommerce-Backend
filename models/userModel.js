2d8d36ac147ffa8fd73ad632cb8e0d7172524bb1
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-08-10 11:38:30 +0530

    added validation

--- models/userModel.js
+++ models/userModel.js
@@ -1,80 +1,93 @@
 const mongoose = require('mongoose'); // Erase if already required
-const bcrypt = require('bcrypt')
-const crypto = require('crypto')
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
+    firstname: {
+        type: String,
+        required: [true, "First name is required"],
+        unique: true,
+        index: true,
+        minlength: [2, "First name must be at least 2 characters long"],
+        maxlength: [50, "First name must be less than 50 characters long"],
     },
-    lastname:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
+    lastname: {
+        type: String,
+        required: [true, "Last name is required"],
+        unique: true,
+        index: true,
+        minlength: [2, "Last name must be at least 2 characters long"],
+        maxlength: [50, "Last name must be less than 50 characters long"],
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            "Please enter a valid email address"
+        ],
     },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
+    mobile: {
+        type: String,
+        required: [true, "Mobile number is required"],
+        unique: true,
+        match: [
+            /^[0-9]{10}$/,
+            "Please enter a valid 10-digit mobile number"
+        ],
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"],
     },
-    role:{
-        type:String,
-        default:'user'
+    role: {
+        type: String,
+        default: 'user'
     },
-    isBlocked:{
-        type:Boolean,
-        default:false
+    isBlocked: {
+        type: Boolean,
+        default: false
     },
-    cart:{
-        type:Array,
-        default:[]
+    cart: {
+        type: Array,
+        default: []
     },
-    address:{
-        type:String,
+    address: {
+        type: String,
     },
-    wishList:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Product'
+    wishList: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
     }],
-    refreshToken:{
-        type:String,
-        
+    refreshToken: {
+        type: String,
     },
-    passwordChangedAt :  Date,
-    passwordResetToken:String,
-    passwordExpires:Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordExpires: Date
 
-},{timestamps:true});
+}, { timestamps: true });
 
-userSchema.pre('save',async function(next){
-    if(!this.isModified("password")){
-        next()
+userSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) {
+        next();
     }
-    const salt = bcrypt.genSaltSync(10)
-    this.password = await bcrypt.hash(this.password,salt)
-})
+    const salt = bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
-userSchema.methods.isPasswordMatched = async function (enterPassword){
-    return await bcrypt.compare(enterPassword,this.password)
-}
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
-userSchema.methods.createPasswordResetToken = async function(){
-    const resetToken = crypto.randomBytes(32).toString('hex')
-    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest("hex")
-    this.passwordExpires = Date.now()+30*60*1000
-    return resetToken
-}
+userSchema.methods.createPasswordResetToken = async function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest("hex");
+    this.passwordExpires = Date.now() + 30 * 60 * 1000;
+    return resetToken;
+};
 
 module.exports = mongoose.model('User', userSchema);
