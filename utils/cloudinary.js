bf455bba70b861bf4eb737a37a1af92f2307d878
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-08-30 13:54:32 +0530

    changed

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,4 +1,4 @@
-const cloudinary = require("cloudinary").v2; // Make sure to use cloudinary.v2 for latest methods
+const cloudinary = require("cloudinary");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -7,38 +7,35 @@ cloudinary.config({
 });
 
 const cloudinaryUploadImg = async (fileToUploads) => {
-  return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload(
-      fileToUploads,
-      { resource_type: "auto" }, // Correctly pass resource_type here
-      (error, result) => {
-        if (error) {
-          reject(error); // Reject promise if there's an error
-        } else {
-          resolve({
-            url: result.secure_url,
-            asset_id: result.asset_id,
-            public_id: result.public_id,
-          });
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(fileToUploads, (result) => {
+      resolve(
+        {
+          url: result.secure_url,
+          asset_id: result.asset_id,
+          public_id: result.public_id,
+        },
+        {
+          resource_type: "auto",
         }
-      }
-    );
+      );
+    });
   });
 };
-
 const cloudinaryDeleteImg = async (fileToDelete) => {
-  return new Promise((resolve, reject) => {
-    cloudinary.uploader.destroy(
-      fileToDelete,
-      { resource_type: "image" }, // Typically "image", ensure correct usage based on your files
-      (error, result) => {
-        if (error) {
-          reject(error); // Reject promise if there's an error
-        } else {
-          resolve(result);
+  return new Promise((resolve) => {
+    cloudinary.uploader.destroy(fileToDelete, (result) => {
+      resolve(
+        {
+          url: result.secure_url,
+          asset_id: result.asset_id,
+          public_id: result.public_id,
+        },
+        {
+          resource_type: "auto",
         }
-      }
-    );
+      );
+    });
   });
 };
 
